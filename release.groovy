8569bf05dce0b2068cfa0fb91ae8cddad2af34ef
James Strachan <james.strachan@gmail.com>
2017-06-19 11:41:58 +0100

    try fix up the git origin

--- release.groovy
+++ release.groovy
@@ -83,9 +83,7 @@ def promoteYamls(releaseVersion) {
     sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
     sh 'chmod 700 /root/.ssh-git'
 
-
-    sh 'git clone git@github.com:fabric8io/fabric8-resources.git'
-    sh 'cd fabric8-resources'
+    git 'git@github.com:fabric8io/fabric8-resources.git'
 
     sh "git config user.email fabric8cd@gmail.com"
     sh "git config user.name fabric8-cd"
